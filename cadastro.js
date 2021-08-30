d0c168b048e55bcb067a93b780c2dda83e6ab580
Ryan <ryangabriel.angelo@gmail.com>
2021-08-30 10:08:48 -0300

    Subindo para repositorio

---
+++ cadastro.js
@@ -0,0 +1,27 @@
+// Data de evento 26/08/2021
+
+var datadevento = 26/08/2021
+if ( datadevento >= 26/08/2021){
+    console.log ("evento disponivel para cadastramento")
+} else 
+    {console.log ("evento invalido")
+ }
+
+ // Idade minima 18
+
+var idade = 18
+if (idade <= 18){
+    console.log ("Idade ok")
+} else 
+    {console.log ("Idade minima 18")
+ }
+
+// Máximo de participantes e 100
+ 
+var participantes = 100
+if (participantes >= 100){
+    console.log ("Cadastro feito")
+} else
+    {console.log ("total de participantes no limite")
+ }
+    
